53db87e884d39b8204dad6e28a10a81a4d23d766
jeikai <bomchip02@gmail.com>
2022-07-21 21:04:17 +0700

    add

--- database.sql
+++ database.sql
@@ -1,19 +1,25 @@
 CREATE DATABASE eProject;
-USE DATABASE eProject;
-CREATE TABLE User(
+USE eProject;
+CREATE TABLE Users(
     userId INT AUTO_INCREMENT PRIMARY KEY,
     phoneNumber VARCHAR(20) NOT NULL,
-    password VARCHAR(100) NOT NULL, --hashed password
-    address TEXT DEFAULt ''
+    password VARCHAR(100) NOT NULL, 
+    address TEXT DEFAULT '',
+    role VARCHAR(10) DEFAULT 'user'
 );
 
-CREATE TABLE Order(
-    orderId INT AUTO_INCREMENT PRIMARY KEY,
-    userId INT, --FK
+CREATE TABLE Orders(
+    orderId INT AUTO_INCREMENT PRIMARY KEY,    
+    userId INT NOT NULL,
     orderDate DATETIME,
     description TEXT DEFAULT ''    
 );
-CREATE TABLE OrderDetail(
+
+ALTER TABLE Orders
+ADD CONSTRAINT FK_Orders_Users
+FOREIGN KEY (userId) REFERENCES Users(userId); 
+
+CREATE TABLE OrderDetails(
     OrderDetailID INT AUTO_INCREMENT PRIMARY KEY,
     orderId INT, --FK
     productId INT, --FK
@@ -21,15 +27,44 @@ CREATE TABLE OrderDetail(
     quantity INT --constraint check: 1->10
 );
 
-CREATE TABLE Product (
+ALTER TABLE OrderDetails
+ADD CONSTRAINT CK_OrderDetails_quantity
+CHECK (quantity > 0 AND quantity <=10); 
+
+
+ALTER TABLE OrderDetails
+ADD CONSTRAINT FK_OrderDetail_Order
+FOREIGN KEY (userId) REFERENCES Users(userId); 
+
+CREATE TABLE Products(
     productId INT PRIMARY KEY,
     weight float NOT NULL,    
     color varchar(20) NOT NULL,
     price float NOT NULL,
     productName VARCHAR(150) NOT NULL,
-    categoryId INT--FK
+    categoryId INT--FK,
+    imageUrls VARCHAR(300),
+    brand VARCHAR(50)
 );
-CREATE TABLE Category (
+
+ALTER TABLE OrderDetails
+ADD CONSTRAINT FK_OrderDetails_Products
+FOREIGN KEY (productId) REFERENCES Products(productId); 
+
+CREATE TABLE Categories (
     categoryId INT PRIMARY KEY,
     categoryName VARCHAR(150) NOT NULL
 );
+
+ALTER TABLE Product
+ADD CONSTRAINT FK_Products_Categories
+FOREIGN KEY (categoryId) REFERENCES Products(categoryId); 
+
+
+INSERT INTO Categories(categoryId, categoryName) VALUES
+(1, 'Electronics'),
+(2, 'Sea foods'),
+(3, 'Fruits');
+
+INSERT INTO Products(productName, color, price,categoryId, imageUrls,brand) VALUES
+('Túi Gucci', 'red', 11.22, 1, '293608736_746568929995037_2608847355166619797_n.jpg;product-1.jpg', 'Gucci')
